ab365d1c4ff90cbd3a374497a8731a6f2443183a
harshith-cs <48485475+harshith-cs@users.noreply.github.com>
2021-10-01 15:43:15 +0530

    Update UV Steriliser.ino

--- UV Steriliser.ino
+++ UV Steriliser.ino
@@ -48,10 +48,10 @@ int checkDoor(){
   int r=0;
   dval = digitalRead(IR);
   if(dval==0){
-    r=5;
+    r=0;
   }
   else{
-    r=10;
+    r=1;
   }
   return r;
 }
@@ -68,7 +68,7 @@ void setup() {
 
 void loop() {
  pstate = "The device is OFF";
-  if(checkDoor() == 5){
+  if(checkDoor() == 0){
     dstate="Door is CLOSED";
   }
   else{
@@ -83,30 +83,31 @@ void loop() {
     endtime = starttime;
     while ((endtime - starttime) <= sec) 
     {
-      if(checkTemp1()<60 && checkTemp2()< 80 && checkDoor()==5){
-     
-      digitalWrite(out2, HIGH);
-       int t1=checkTemp1();
-  int t2=checkTemp2();
-      Serial.print(t1);
-      Serial.print("C");
-      Serial.print("|");
-  Serial.print(t2); 
-  Serial.print("C");
-  Serial.print("|");
-  Serial.print(pstate);
-  Serial.print("|");
-  Serial.println(dstate);
-      loopcount = loopcount + 1;
-      endtime = millis();
-      pstate = "The device is ON";
+      if(checkTemp1()<60 && checkTemp2()< 80 && checkDoor()==0)
+      {
+        digitalWrite(out2, HIGH);
+        int t1=checkTemp1();
+        int t2=checkTemp2();
+        Serial.print(t1);
+        Serial.print("C");
+        Serial.print("|");
+        Serial.print(t2); 
+        Serial.print("C");
+        Serial.print("|");
+        Serial.print(pstate);
+        Serial.print("|");
+        Serial.println(dstate);
+        loopcount = loopcount + 1;
+        endtime = millis();
+        pstate = "The device is ON";
       }
-      else{
+     else
+     {
       digitalWrite(out1, LOW);
       digitalWrite(out2, LOW);
       pstate = "The device is OFF";
       break;
-      }
+     }
     }
     digitalWrite(out1, LOW);
     digitalWrite(out2, LOW);
